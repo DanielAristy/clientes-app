c94849fc1074893fb3c70d259048a754f9084be8
DanielAristy <danielariaty@gmail.com>
2019-11-26 01:26:11 -0500

    Forma de integrar Boostrap de una manera sencilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bienvenido a Angular!';
-  curso: string = 'Curso de Spring 5 con Angular 7';
-  profesor: string = 'Daniel Aristizabal';
+  curso  = 'Curso de Spring 5 con Angular 7';
+  profesor = 'Daniel Aristizabal';
 }
